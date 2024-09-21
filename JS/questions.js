506237a626504965cbf357d6ef57bc4b1ac592d3
Emily Souza <emilyssouza05@gmail.com>
2024-09-21 15:12:27 -0300

    feat: Adding the question array

---
+++ JS/questions.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        question: "Naruto usa roupas de que cor na temporada Shippuuden?",
+        answers: [
+            { option: "Laranja e preto", value: 3, correct: true },
+            { option: "Preto e azul", value: 0, correct: false },
+            { option: "Verde e preto", value: 0, correct: false },
+            { option: "Preto e prateado", value: 0, correct: false }
+        ]
+    }
+
+]
