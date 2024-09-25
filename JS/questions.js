d38014bcdac2c67e55a9aeca19dbeaa5b56d661d
davi <davinunes07@gmail.com>
2024-09-24 22:01:11 -0300

    feat:I added questions in the JavaScript

--- JS/questions.js
+++ JS/questions.js
@@ -6,7 +6,36 @@ export default [
             { option: "Preto e azul", value: 0, correct: false },
             { option: "Verde e preto", value: 0, correct: false },
             { option: "Preto e prateado", value: 0, correct: false }
-        ]
+        ],
+
+        question: " Qual o nome do criador de Naruto? ",
+        answers: [
+            { option: "akira toriyama", value: 0, correct: false},
+            { option: "junko takeuchi", value: 0, correct: false},
+            { option: "masashi kishimoto", value: 3, correct: true},
+            { option: "seishi kishimoto", value: 0, correct: false},
+
+           
+        ],
+
+        question: "  Quem matou Zabuza momochi?  ",
+        answers: [
+            { option: "naruto", value: 0, correct: false},              
+            { option: "sasuke", value: 0, correct: false},
+            { option: "kakashi", value: 0, correct: false},
+            { option: "ninguem", value: 3, correct: true},
+         ],
+
+         question: "   Quantos dias naruto levou pra aprender o rasengan?  ",
+        answers: [
+            { option: "34 dias", value: 0, correct: false},
+            { option: "37 dias", value: 3, correct: true},
+            { option: "90 dias", value: 0, correct: false},
+            { option: "40 dias", value: 0, correct: false},
+         ], 
+
     }
+
+
 
 ]
