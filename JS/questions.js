adb807b5d34f7d4824de4d61250049533c8334f2
Emily Souza <emilyssouza05@gmail.com>
2024-10-03 18:44:29 -0300

    feat: Added more questions and some css adjustments

--- JS/questions.js
+++ JS/questions.js
@@ -2,39 +2,66 @@ const arrayQuestions =  [
     {
         question: "Naruto usa roupas de que cor na temporada Shippuuden?",
         answers: [
-            { option: "Laranja e preto", value: 3, correct: true },
-            { option: "Preto e azul", value: 0, correct: false },
-            { option: "Verde e preto", value: 0, correct: false },
-            { option: "Preto e prateado", value: 0, correct: false }
+            { option: "Laranja e preto", correct: true },
+            { option: "Preto e azul", correct: false },
+            { option: "Verde e preto", correct: false },
+            { option: "Preto e prateado", correct: false }
         ]
     },
     {
         question: "Qual o nome do criador de Naruto?",
         answers: [
-            { option: "Akira Toriyama", value: 0, correct: false },
-            { option: "Junko Takeuchi", value: 0, correct: false },
-            { option: "Masashi Kishimoto", value: 3, correct: true },
-            { option: "Seishi Kishimoto", value: 0, correct: false }
+            { option: "Akira Toriyama", correct: false },
+            { option: "Junko Takeuchi", correct: false },
+            { option: "Masashi Kishimoto", correct: true },
+            { option: "Seishi Kishimoto", correct: false }
         ]
     },
     {
         question: "Quem matou Zabuza Momochi?",
         answers: [
-            { option: "Naruto", value: 0, correct: false },
-            { option: "Sasuke", value: 0, correct: false },
-            { option: "Kakashi", value: 0, correct: false },
-            { option: "Ninguém", value: 3, correct: true }
+            { option: "Naruto", correct: false },
+            { option: "Sasuke", correct: false },
+            { option: "Kakashi", correct: false },
+            { option: "Ninguém", correct: true }
         ]
     },
     {
         question: "Quantos dias Naruto levou pra aprender o Rasengan?",
         answers: [
-            { option: "34 dias", value: 0, correct: false },
-            { option: "37 dias", value: 3, correct: true },
-            { option: "90 dias", value: 0, correct: false },
-            { option: "40 dias", value: 0, correct: false }
+            { option: "34 dias", correct: false },
+            { option: "37 dias", correct: true },
+            { option: "90 dias", correct: false },
+            { option: "40 dias", correct: false }
         ]
-    }
+    },
+    {
+        question: "Qual o objetivo da Akatsuki?",
+        answers: [
+            { option: "Matar o Orochimaru", correct: false },
+            { option: "Destruir a Vila da Folha", correct: false },
+            { option: "Capturar todas as bijuus", correct: true },
+            { option: "Capturar todos os Kages", correct: false }
+        ]
+    },
+    {
+        question: "Entre esses, qual tem o Sharingan?",
+        answers: [
+            { option: "Tobi", correct: true },
+            { option: "Hidan", correct: false },
+            { option: "Zetsu", correct: false },
+            { option: "Kisame", correct: false }
+        ]
+    },
+    {
+        question: "Qual o significado do nome Sakura? ",
+        answers: [
+            { option: "Flor de Amora", correct: false },
+            { option: "Flor de Macieira", correct: false },
+            { option: "Flor de Cerejeira", correct: true },
+            { option: "Flor de Lótus", correct: false }
+        ]
+    },
 ];
 
 export default arrayQuestions;
